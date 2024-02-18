8d627ed03bd869177376916ec9cff54847ce70ac
Mahdi Shahmoradi Ramsheh <119459242+Mahdi-Shah@users.noreply.github.com>
2024-02-18 18:37:01 +0330

    complete EFCore practice

---
+++ EFCore/Model.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog>? Blogs { get; set; }
+    public DbSet<Post>? Posts { get; set; }
+
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseNpgsql("User ID=postgres;Password=1234;" +
+                             "Host=localhost;Port=5432;Database=staracademy;" +
+                             "Connection Lifetime=0;");
+}
+
+public class Blog
+{
+    public string BlogId { get; set; }
+    public string? Url { get; set; }
+
+    public List<Post>? Posts { get; } = new();
+}
+
+public class Post
+{
+    
+    public string PostId { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog? Blog { get; set; }
+}
---
+++ EFCore/Program.cs
@@ -0,0 +1,35 @@
+﻿using var db = new BloggingContext();
+
+// Create
+Console.WriteLine("Inserting a new blog");
+Blog firstBlog = new Blog
+{
+    BlogId = Guid.NewGuid().ToString(),
+    Url = "http://blogs.msdn.com/adonet"
+};
+
+db.Add(firstBlog);
+db.SaveChanges();
+
+// Read
+Console.WriteLine("Querying for a blog");
+var blog = db.Blogs
+    .OrderBy(b => b.BlogId)
+    .First();
+
+// Update
+Console.WriteLine("Updating the blog and adding a post");
+blog.Url = "https://devblogs.microsoft.com/dotnet";
+Post post = new Post
+{
+    Title = "Hello World",
+    Content = "I wrote an app using EF Core!",
+    PostId = Guid.NewGuid().ToString(),
+};
+blog.Posts!.Add(post);
+db.SaveChanges();
+
+// Delete
+Console.WriteLine("Delete the blog");
+db.Remove(blog);
+db.SaveChanges();
